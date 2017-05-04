48731ce7f0617418b933d879dc3d862a39325847
pierrealbiges_apple <dearfriends@live.fr>
2017-05-04 16:57:58 +0200

    Uploading a py version

---
+++ 2017-05-04_networking_clientside.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import io
+import socket
+import struct
+import time
+import picamera
+
+client.socket = socket.socket()
+client.socket.connect(('Ubunte_server',8000))
+
+connection = client_socket.makefile('wb')
+try:
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    
+    camera.start_preview()
+    time.sleep(2)
+    
+    start = time.time()
+    stream = io.BytesIO()
+    for capture in camera.capture_continuous(stream, 'jpeg'):
+        connection.write(struct.pack('<L', stream.tell()))
+        connection.flush
+        
+        stream.seek(0)
+        connection.write(stream.read())
+        
+        if time.time() - start > 30:
+            break
+        
+        stream.seek(0)
+        stream.truncate()
+        
+    connection.write(struct.pack('<L', 0))
+
+finally:
+    connection.close()
+    client_socket.close()
+

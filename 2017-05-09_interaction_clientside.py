2f75b78b0a66655e864bde6c99465109d88120b4
pierrealbiges_apple <dearfriends@live.fr>
2017-05-09 17:03:53 +0200

    Trying to add picamera to the interaction scripts

--- 2017-05-09_interaction_clientside.py
+++ 2017-05-09_interaction_clientside.py
@@ -1,13 +1,31 @@
 import time
 import zmq
+import picamera
+import struct
+import io
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
+print('Server connected')
 
 while True:
     message = socket.recv() #Wait for next request from client
     print("Received request: %s" % message)
+    
+    if message == 'END':
+        break
+    
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    
+    start = time.time()
+    stream = io.BytesIO()
+    for capture in camera.capture_continuous(stream, 'jpeg'):
+        time.sleep(1)
 
-    time.sleep(2)
-    socket.send(b"World") #Send reply back to client
+        socket.send(struct.pack('<L', stream.tell()))
+        
+        if time.time() - start > 10:
+            break

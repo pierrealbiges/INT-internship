cde8b8d4b0245bc456140440f1f8700865e695d1
pierrealbiges_apple <dearfriends@live.fr>
2017-05-10 11:25:09 +0200

    Updating : working on the image encoding problem

--- 2017-05-10_interaction_clientside.py
+++ 2017-05-10_interaction_clientside.py
@@ -9,28 +9,31 @@ socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 print('Client connected')
 
-while True:
-	message = socket.recv() #Wait for next request from client
-	print("Received request: %s" % message)
+try:
+	while True:
+		message = socket.recv() #Wait for next request from client
+		print("Received request: %s" % message)
 
-	if message == 'END':
-		break
-
-	camera = picamera.PiCamera()
-	camera.resolution = (640, 480)
-	camera.start_preview()
-	time.sleep(2)
-
-	start = time.time()
-	stream = io.BytesIO()
-	for capture in camera.capture_continuous(stream, 'jpeg'):
-
-		socket.send(stream.read())
-		#socket.send(struct.pack('<L', stream.tell()))
-
-		if time.time() - start > 5:
-			socket.close()
+		if message == 'END':
 			break
 
-socket.close()
-Context.term()
+		camera = picamera.PiCamera()
+		camera.resolution = (640, 480)
+		camera.start_preview()
+		time.sleep(2)
+
+		start = time.time()
+		stream = io.BytesIO()
+		for capture in camera.capture_continuous(stream, 'jpeg'):
+
+			socket.send(stream.read())
+			#socket.send(struct.pack('<L', stream.tell()))
+
+			if time.time() - start > 5:
+				socket.close()
+				break
+
+finally:
+	socket.close()
+	context.term()
+	print('Socket and context closed')

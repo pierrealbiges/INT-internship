ab9c83291269d1779ad9824ea7fb3d018a6d8f7e
pierrealbiges_apple <dearfriends@live.fr>
2017-05-10 13:53:54 +0200

    Updating : working on the empty string reply

--- 2017-05-10_interaction_clientside.py
+++ 2017-05-10_interaction_clientside.py
@@ -11,19 +11,22 @@ socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 print('Server connected')
 
+while True:
+    message = socket.recv() #Wait for next request from client
+    print("Received request: %s" % message)
+    
+    if message == 'END':
+        break
 
-message = socket.recv() #Wait for next request from client
-print("Received request: %s" % message)
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    time.sleep(2)
 
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.start_preview()
-time.sleep(2)
+    stream = io.BytesIO()
 
-stream = io.BytesIO()
-
-socket.send(stream.read())
-#socket.send(struct.pack('<L', stream.tell()))
+    socket.send(stream.read())
+    #socket.send(struct.pack('<L', stream.tell()))
 
 
 socket.close()

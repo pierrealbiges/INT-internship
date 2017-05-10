9aac2dccd587a3e0ba144132a722db3aad088782
pierrealbiges_apple <dearfriends@live.fr>
2017-05-10 17:12:36 +0200

    Updating : working on the empty string reply

--- 2017-05-10_interaction_clientside.py
+++ 2017-05-10_interaction_clientside.py
@@ -14,33 +14,33 @@ print('Server connected')
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 
-try:
-    while True:
-        message = socket.recv() #Wait for next request from client
-        print("Received request: %s" % message)
 
-        if message == 'END':
-            break
+while True:
+	message = socket.recv() #Wait for next request from client
+	print("Received request: %s" % message)
 
-        stream = io.BytesIO()
-        camera.start_preview()
-        time.sleep(1)
-        
-        camera.capture(stream, format='jpeg')
-        print('dbkey n1', len(stream.getvalue()))
-	
+	if message == 'END':
+		break
+
+	stream = io.BytesIO()
+	camera.start_preview()
+	time.sleep(1)
+
+	camera.capture(stream, format='jpeg')
+	print('dbkey n1', len(stream.getvalue()))
+
 	print('dbkey n2', len(stream))
 
 	reply = stream.read()
 
-        socket.send(reply)
+	socket.send(reply)
 
 	print('dbkey n3', reply)
-        #socket.send(struct.pack('<L', stream.tell()))
+	#socket.send(struct.pack('<L', stream.tell()))
 
 	time.sleep(1)
 
-except:
-    socket.close()
-    context.term()
-    print('Socket and context closed')
+
+socket.close()
+context.term()
+print('Socket and context closed')

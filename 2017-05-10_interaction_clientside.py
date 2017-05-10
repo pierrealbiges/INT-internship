bc38d8c1fac2b97a83545776549f6e38dc02dad2
pierrealbiges_apple <dearfriends@live.fr>
2017-05-10 10:40:15 +0200

    Updating : working on the image encoding problem

--- 2017-05-10_interaction_clientside.py
+++ 2017-05-10_interaction_clientside.py
@@ -10,22 +10,22 @@ socket.bind("tcp://*:5555")
 print('Client connected')
 
 while True:
-    message = socket.recv() #Wait for next request from client
-    print("Received request: %s" % message)
-    
-    if message == 'END':
-        break
-    
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    
-    start = time.time()
-    stream = io.BytesIO()
-    for capture in camera.capture_continuous(stream, 'jpeg'):
+	message = socket.recv() #Wait for next request from client
+	print("Received request: %s" % message)
 
-        socket.send(struct.pack('<L', stream.read())
-#        socket.send(struct.pack('<L', stream.tell()))
-        
-        if time.time() - start > 5:
-            break
+	if message == 'END':
+		break
+
+	camera = picamera.PiCamera()
+	camera.resolution = (640, 480)
+	camera.start_preview()
+
+	start = time.time()
+	stream = io.BytesIO()
+	for capture in camera.capture_continuous(stream, 'jpeg'):
+
+		socket.send(struct.pack('<L', stream.read())
+		#socket.send(struct.pack('<L', stream.tell()))
+
+		if time.time() - start > 5:
+			break

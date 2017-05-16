56d917bdfaf17cec128c725117dbfbd420f2d70a
pierrealbiges_apple <dearfriends@live.fr>
2017-05-16 13:59:20 +0200

    updating *_capture_videos_with_openRetina.py

--- 2017-05-16_capture_videos_with_openretina.py
+++ 2017-05-16_capture_videos_with_openretina.py
@@ -5,7 +5,7 @@ import picamera
 
 picam = picamera.PiCamera
 picam.resolution = (640,480) #Defining the camera resolution
-picam.start_recording(output='video_picam', format='h264') #Start recording in 'filename.filetype'
+picam.start_recording(self,output='video_picam', format='h264') #Start recording in 'filename.filetype'
 picam.wait_recording(10) #Better integrated time.wait() that allow to continually check for errors
 picam.stop_recording() #Stop recording
 

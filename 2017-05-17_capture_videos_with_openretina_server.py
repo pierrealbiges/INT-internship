f57b6005d2b3e82072bab762d76d68cd11440fcd
pierrealbiges_apple <dearfriends@live.fr>
2017-05-17 17:17:35 +0200

    Updating : working on data merging

--- 2017-05-17_capture_videos_with_openretina_server.py
+++ 2017-05-17_capture_videos_with_openretina_server.py
@@ -1,21 +1,33 @@
-""" Server side """
+""" Computer side """
 
 import zmq
-import struct
-import io
 
-context = zmq.Context()
-print('Connecting to Pi...')
-socket = context.socket(zmq.REQ)
-socket.connect('tcp://10.164.7.213:5555')
-print('Connected')
+try: 
+    context = zmq.Context()
+    print('Connecting to Pi...')
+    dealer = context.socket(zmq.DEALER)
+    dealer.connect('tcp://10.164.7.213:5555')
+    print('Connected')
+    
+    CHUNCK_SIZE = 250000
+    print('Sending request')
+    dealer.send(b'fetch')
+    
+    total = 0
+    chuncks = 0
+    
+    while True:
+        chunck = dealer.recv()
+        chuncks += 1
+        size = len(chunck)
+        total += size
+    
+        if size == 0:
+            break #Whole file received
+            
+    print('%i chuncks received, %i bytes' % (chuncks, total))
 
-print('Sending request')
-socket.send(b'Camera request')
-
-message = socket.recv()
-print('Received reply [ %s ]' % (len(message)))
-
-socket.close()
-context.term()
-print('Connection closed')
+finally: 
+    dealer.close()
+    context.term()
+    print('\nConnection closed')

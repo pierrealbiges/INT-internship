a35e60a4a1c4ce9f04cacbc0f66ede2e67a60285
pierrealbiges_apple <dearfriends@live.fr>
2017-05-17 09:41:50 +0200

    updating *_capture_videos_with_openRetina.py

--- 2017-05-17_capture_videos_with_openretina.py
+++ 2017-05-17_capture_videos_with_openretina.py
@@ -6,14 +6,11 @@ import picamera
 class Rec_video():
     def __init__(self):
         cam = picamera.PiCamera()
-        cam.resolution = (640,480) #Defining the camera resolution
-        
-        print('Recording start') #Control line
-        
+        cam.resolution = (640,480) #Defining the camera resolution        
         cam.start_recording(output='video_picam2', format='h264') #Start recording in 'filename.filetype'
         cam.wait_recording(5) #Better integrated time.wait() that allow to continually check for errors
         cam.stop_recording() #Stop recording
 
-        print('\nRecording end') #Control line
+        print('Recording end') #Control line
         
 rec = Rec_video() #Calling the Rec_video class

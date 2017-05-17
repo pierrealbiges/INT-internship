efa001c4998bb0312d50a6c8e8615a8cd40fb3e0
pierrealbiges_apple <dearfriends@live.fr>
2017-05-17 14:36:10 +0200

    updating *_capture_videos_with_openRetina_client.py

--- 2017-05-17_capture_videos_with_openretina_client.py
+++ 2017-05-17_capture_videos_with_openretina_client.py
@@ -10,14 +10,25 @@ print("Connected to computer")
 
 try:
     file = open("video_picam","r")
+    print('File opened')
     while True:
         identity, command = router.recv_multipart()
+        
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                return
+            else:
+                raise
+        
         assert command == b"fetch"
+        print('Command received')
         
         while True:
             data = file.read(CHUNCK_SIZE)
             router.send_multipart([identity, data])
+            print('Sending data...')
             if not data:
+                print('\nNo more data')
                 break
 
 finally:

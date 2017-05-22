54e1447b4707326ce6334eb1eb92a3857df0b6ae
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-05-22 11:07:19 +0200

    eventRetina : multi-layer (but synchronous - driven by the frames)

--- 2017-05-12_using_openretina_testing_noise_srv.py
+++ 2017-05-12_using_openretina_testing_noise_srv.py
@@ -8,7 +8,7 @@ import subprocess
 p = subprocess.Popen(['./2017-05-12_using_openretina_testing_noise.py'])
 
 from openRetina import openRetina
-noise = openRetina(model=dict(layer='displaying noise that we receive through the network', # label for this layer
+noise = openRetina(model=dict(layer='output', title='displaying noise that we receive through the network', # label for this layer
                                  input=['stream'], # input: can be the camera, noise, a movie (TODO)
                                  output=['display'], # output: can be stream, display, capture,...
                                  T_SIM=4))

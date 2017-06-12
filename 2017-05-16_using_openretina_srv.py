eaf459c1597a8cd2c67f83325527d28887c10960
Laurent Perrinet <laurent.perrinet@gmail.com>
2017-06-12 09:53:05 +0200

    updating *_using_openretina.py

--- 2017-05-16_using_openretina_srv.py
+++ 2017-05-16_using_openretina_srv.py
@@ -4,8 +4,11 @@ See https://github.com/laurentperrinet/openRetina
 """
 __author__ = "(c) Victor Boutin & Laurent Perrinet INT - CNRS"
 
+import subprocess
+p = subprocess.Popen(['ssh pi@10.164.7.213 "cd ~/INT-internship ; python3 2017-05-16_using_openretina.py "'])
+
 from openRetina import openRetina
-thalamus = openRetina(model=dict(layer='thalamus', # label for this layer
+thalamus = openRetina(model=dict(layer='thalamus', title='A simple interaction with the pi retina', # label for this layer
                                  ip='10.164.7.213',
                                  input=['stream'], # input: can be the camera, noise, a movie (TODO)
                                  output=['display'], # output: can be stream, display, capture,...
